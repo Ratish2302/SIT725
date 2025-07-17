1f71a5963fb9bc21ebbc753a3f91830c86494514
Ratish2302 <sharmaratish02@gmail.com>
2025-07-17 14:26:43 +1000

    First commit

--- Week1/JS/script.js
+++ Week1/JS/script.js
@@ -2,6 +2,7 @@ function changeText(){
 var textsArray = ["Text 1","Text 2", "Text 3", "Text 4", "Text 5"]
 var number = getRandomNumberBetween(0,textsArray.length - 1)
 console.log("Index: ", number)
+console.log('This is logging')
 document.getElementById("heading").innerHTML = textsArray[number];
 }
 function getRandomNumberBetween(min,max){
